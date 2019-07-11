de5eef47971891a6817d419d28e4f58922bb9c06
estacao hack <cursomastertech@gmail.com>
2019-07-11 11:08:39 -0300

    exercicio dos degraus

---
+++ exerc.js
@@ -0,0 +1,12 @@
+
+
+
+let lances = prompt('Lances: ')
+let degrau = ''
+
+let i=0
+while(degrau.length <= lances){
+    degrau += '#'
+    console.log(degrau)
+    i++
+}
--- script.js
+++ script.js
@@ -5,3 +5,4 @@ let nota = Math.round(parseFloat(prompt('Nota do Aluno: ').replace(',','.')))
 nota >= 7 ?
 alert(`Parabens ${nome} Voce passou.. Com a nota ${nota}!!`):
 alert(`Que Pena, ${nome}, Voce não Passou em ${materia} Sua nota ${nota} esta abaixo !`)
+
